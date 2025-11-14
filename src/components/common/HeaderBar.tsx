8f1f3061aa3a7898c071234e705c3cad0ef01c8e
ohsung0722 <99doldol@naver.com>
2025-11-15 03:41:40 +0900

    feat(component): 공통으로 사용할 HeaderBar 컴포넌트 구현

---
+++ src/components/common/HeaderBar.tsx
@@ -0,0 +1,45 @@
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { COLORS, FONT, SPACING } from "../../constants/theme";
+
+interface HeaderBarProps {
+  title: string;
+}
+
+function HeaderBar({ title }: HeaderBarProps) {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={() => navigation.goBack()}>
+        <Ionicons name="arrow-back" size={24} color={COLORS.white} />
+      </Pressable>
+
+      <Text style={styles.title}>{title}</Text>
+
+      <View style={styles.blank} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    paddingVertical: SPACING.md,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontSize: FONT.lg,
+    color: COLORS.white,
+    fontWeight: "700",
+  },
+  blank: {
+    width: 24,
+  },
+});
+
+export default HeaderBar;
