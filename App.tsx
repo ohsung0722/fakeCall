889f1d6580e80c8e0cc6cff8604b9bebfb097470
ohsung0722 <99doldol@naver.com>
2025-11-15 23:51:38 +0900

    feat(App.tsx): NavigationContainer 설정

--- App.tsx
+++ App.tsx
@@ -2,16 +2,19 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Header from "./src/components/FakeCallScreen/Header";
 import FakeCallScreen from "./src/screens/FakeCallScreen";
+import ContactListScreen from "./src/screens/ContactListScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <FakeCallScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={FakeCallScreen} />
+        <Stack.Screen name="Contacts" component={ContactListScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
